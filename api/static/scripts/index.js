f390d240914fe6f37402325769cd72e34df77d5e
IyasuH <eyasuhailegbr@gmail.com>
2023-09-24 11:16:28 +0300

    update

--- api/static/scripts/index.js
+++ api/static/scripts/index.js
@@ -1,4 +1,4 @@
 document.getElementById('webview_data').innerHTML=JSON.stringify(window.Telegram.WebApp.initDataUnsafe.user, null, 2);
 document.getElementById('welcome_by_name').innerHTML="Hello " + window.Telegram.WebApp.initDataUnsafe.user.first_name;
 // document.getElementById('user_id').innerHTML=window.Telegram.WebApp.initDataUnsafe.id;
-document.getElementById("userId").value=window.Telegram.WebApp.initDataUnsafe.id;
+document.getElementById("userId").value=window.Telegram.WebApp.initDataUnsafe.user.id;
