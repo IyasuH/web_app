1e7feed1d49ff5dea469165dcfaab2dc2e678fad
IyasuH <eyasuhailegbr@gmail.com>
2023-09-24 12:46:25 +0300

    update

--- api/index.py
+++ api/index.py
@@ -17,11 +17,11 @@ def home():
     to edit perosoanl data
     """
     msg = "did not get user_id"
-    if request.method == 'POST' and 'user_id' in request.form:
-        user_id = request.form.get("user_id")
-        user_name = request.form.get("userName")
-        
-        msg ="user id is {} and user name {}".format(user_id, user_name)
+    # if request.method == 'POST' and 'user_id' in request.form:
+    user_id = request.form.get("user_id")
+    user_name = request.form.get("userName")
+
+    msg ="user id is {} and user name {}".format(user_id, user_name)
 
     return render_template('home.html', msg=msg)
 
--- api/static/scripts/index.js
+++ api/static/scripts/index.js
@@ -2,7 +2,8 @@ userData = window.Telegram.WebApp.initDataUnsafe.user;
 
 document.getElementById('webview_data').innerHTML=JSON.stringify(userData, null, 2);
 document.getElementById('welcome_by_name').innerHTML="Hello " + userData.first_name;
-document.getElementById('user_id').innerHTML=userData.id;
+// document.getElementById('user_id').innerHTML=userData.id;
+document.getElementById("userId").value = userData.id;
 document.getElementById('userName').value = userData.username;
 
 // document.getElementById("userId").value=window.Telegram.WebApp.initDataUnsafe.user.id;
