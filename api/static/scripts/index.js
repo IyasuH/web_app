1a413c537bd43fa591db545c67b90deb465c23ad
IyasuH <eyasuhailegbr@gmail.com>
2023-09-28 15:00:38 +0300

    update

--- api/static/scripts/index.js
+++ api/static/scripts/index.js
@@ -1,10 +1,13 @@
-userData = window.Telegram.WebApp.initDataUnsafe.user;
+// userData = window.Telegram.WebApp.initDataUnsafe.user;
 
-document.getElementById("userId").value = userData.id;
-document.getElementById('userName').value = userData.username;
+// document.getElementById("userId").value = userData.id;
+// document.getElementById('userName').value = userData.username;
 
-document.getElementById('webview_data').innerHTML=JSON.stringify(userData, null, 2);
-document.getElementById('welcome_by_name').innerHTML="Hello " + userData.first_name;
-// document.getElementById('user_id').innerHTML=userData.id;
+// document.getElementById('webview_data').innerHTML=JSON.stringify(userData, null, 2);
+// document.getElementById('welcome_by_name').innerHTML="Hello " + userData.first_name;
 
-// document.getElementById("userId").value=window.Telegram.WebApp.initDataUnsafe.user.id;
+const TelegramWebApp = require('telegram-web-app');
+
+// Get the user ID.
+const userId = TelegramWebApp.getUser().id;
+document.getElementById("userId").value = userId;
